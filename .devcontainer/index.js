de1ea4555a121155507dcb37369d0d30a226e0b9
VYOM PUNDHIR <119744177+vyomPundhir@users.noreply.github.com>
2023-04-20 18:06:46 +0000

    Index.js file added.

---
+++ .devcontainer/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import app from "./src/app.js";
+
+( async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/ecomm")
+        console.log("DB Connected !");
+
+        app.on('error',(err)=>{
+            console.error("ERROR: ", err);
+            throw err 
+        })
+
+        const onListening = () => {
+            console.log(`Listening on port 5000`);
+        }
+
+        app.listen(5000, onListening)
+
+    } catch (err) {
+        console.error("ERROR: ", err)
+        throw err
+    }
+})()
